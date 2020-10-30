3b8b86f2e6098b0667b771a9d1e27a4fb8fae6a9
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-30 01:32:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-const fi = (function() {
-  return {
-    libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
-    },
+const fi = (function () {
+  return   {
+   libraryMethod: function () {
+     return "Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0";
+   },
+   each: function (collection, alert) {
 
-    each: function(collection, alert) {
-for (const c in collection) {
+      for (const c in collection) {
         alert(collection[c]);
       }
       return collection;
-    },
+   },
+   map: function (collection, callback) {
 
-    map: function(collection, callback) {
-let array = [];
+      let array = [];
       for (const c in collection) {
         array.push(callback(collection[c]));
       }
       return array;
-    },
+   },
+   reduce: function (collection, callback, acc) {
 
-    reduce: function(collection, callback, acc) {
- if (acc === undefined) {
+      if (acc === undefined) {
         return collection.reduce(callback);
       } else {
         return collection.reduce(callback, acc);
       }
     },
-
-    functions: function(collection, predicate) {
-for (const c in collection) {
+    find: function (collection, predicate) {
+      for (const c in collection) {
         if (predicate(collection[c]) === true) {
           return collection[c];
         }
       }
     },
-
-filter: function (collection, predicate) {
+    filter: function (collection, predicate) {
       const newArr = [];
       for (const c in collection) {
         if (predicate(collection[c]) === true) {
